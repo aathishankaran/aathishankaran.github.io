c126f1a012f3e47e391b2901b0cbacbe3d980031
ALAGUSHANKAR <aathishankaran@gmail.com>
2025-01-19 17:41:45 -0600

    initial commit

---
+++ src/components/partials/AboutSection.tsx
@@ -0,0 +1,102 @@
+import Button from '@/components/form/Button';
+import SectionTitle from '@/components/shared/SectionTitle';
+import { FiCoffee } from 'react-icons/fi';
+import { HiOutlineChartBar, HiOutlineFire, HiOutlineUsers } from 'react-icons/hi';
+import ProgressBar from '@/components/shared/ProgressBar';
+
+const AboutSection = () => {
+  return (
+    <>
+      <SectionTitle>About Me</SectionTitle>
+
+      <div className="grid grid-cols-1 gap-6 py-6 md:grid-cols-2 lg:gap-8">
+        {/* Bio */}
+        <div className="">
+          <p className="text-justify">
+            Seasoned Data Engineer with over 17 years of expertise in designing and implementing robust data architectures and solutions to drive strategic business objectives. Proven ability to lead architectural design for complex, cross-functional projects, delivering scalable and reusable components to optimize development efforts. Experienced in collaborating across teams to align data strategies with organizational goals, ensuring scalability, security, and performance.
+          </p>
+          <Button className="mt-5">Download CV</Button>
+        </div>
+
+        {/* Skills */}
+        <div className="grid grid-cols-2 gap-x-6 gap-y-4">
+          <div className="">
+            <div className="mb-3 flex justify-between">
+              <h6 className="font-semibold">Spring Boot</h6>
+              <p>85%</p>
+            </div>
+            <ProgressBar color="blue" progress={85} />
+          </div>
+          <div className="">
+            <div className="mb-3 flex justify-between">
+              <h6 className="font-semibold">Angular</h6>
+              <p>45%</p>
+            </div>
+            <ProgressBar color="amber" progress={95} />
+          </div>
+
+          <div className="">
+            <div className="mb-3 flex justify-between">
+              <h6 className="font-semibold">Big Data</h6>
+              <p>65%</p>
+            </div>
+            <ProgressBar color="rose" progress={75} />
+          </div>
+          <div className="">
+            <div className="mb-3 flex justify-between">
+              <h6 className="font-semibold">Data Engineering</h6>
+              <p>65%</p>
+            </div>
+            <ProgressBar color="green" progress={80} />
+          </div>
+        </div>
+      </div>
+
+      {/* Overview */}
+      <div className="mt-10 grid grid-cols-2 gap-4 md:grid-cols-4">
+        <div className="flex">
+          <div className="mr-5 text-gray-300">
+            <HiOutlineChartBar size={50} />
+          </div>
+          <div>
+            <h2 className="text-3xl font-bold">17+</h2>
+            <p className="mt-1">Years of experience</p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div className="mr-5 text-gray-300">
+            <HiOutlineFire size={50} />
+          </div>
+          <div>
+            <h2 className="text-3xl font-bold">20+</h2>
+            <p className="mt-1">Projects Completed</p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div className="mr-5 text-gray-300">
+            <HiOutlineUsers size={50} />
+          </div>
+          <div>
+            <h2 className="text-3xl font-bold">10+</h2>
+            <p className="mt-1">clients</p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div className="mr-5 text-gray-300">
+            <FiCoffee size={50} />
+          </div>
+          <div>
+            <h2 className="text-3xl font-bold">2+</h2>
+            <p className="mt-1">Countries</p>
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default AboutSection;
---
+++ src/data/experiences.ts
@@ -0,0 +1,42 @@
+import { Experience } from '@/types';
+
+export const experiences: Experience[] = [
+  {
+    jobTitle: 'Technical Architect',
+    company: 'Accenture',
+    startDate: 'July 2021',
+    endDate: 'Present',
+    description: 'Led the design, development, and configuration of applications, serving as the primary point of contact for technical decisions and stakeholder communication.',
+    company_logo:'/images/company_logo/accenture.png'
+  },
+
+  {
+    jobTitle: 'Technology Specialist',
+    company: 'Capital One',
+    startDate: 'Jun 2016',
+    endDate: 'May 2021',
+    description: 'Developed scalable and efficient presentation layers using frameworks such as AWS, Apache Spark, and Apache Kafka, ensuring seamless data processing and integration.',
+    company_logo:'/images/avatar/avatar.png'
+
+  },
+
+  {
+    jobTitle: 'Technical Lead',
+    company: 'Cognizant',
+    startDate: 'Mar 2013',
+    endDate: 'May 2021',
+    description: 'Led the creation of high-level architectural designs for automation code in DW transformations. Spearheaded the development and implementation of automated code generation for Informatica mappings.',
+    company_logo:'/images/avatar/avatar.png'
+
+  },
+
+  {
+    jobTitle: 'Analyst',
+    company: 'Verizon',
+    startDate: 'Jul 2010',
+    endDate: 'Mar 2013',
+    description: 'Senior Developer Role: Led the development of voice portal applications for Verizon customers, enabling seamless access to billing services through an intuitive interface.',
+    company_logo:'/images/avatar/avatar.png'
+
+  },
+];
